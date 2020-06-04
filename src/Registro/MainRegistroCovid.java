5f157454ce6bd706a21bb3b5d2a2753d99ad5d64
frangusdj <frangusdj@hotmail.com>
2020-06-03 22:26:51 -0500

    Digrama inverso UML

--- src/Registro/MainRegistroCovid.java
+++ src/Registro/MainRegistroCovid.java
@@ -1,6 +1,11 @@
 package registro;
 public class MainRegistroCovid {
     public static void main(String[] args) {
-        
+        String NombreCanton="";
+        int nContagiados,nDecesos,nHospitalizados,nDescartados,nRecuperados,nTPruebas,op;
+        nContagiados=nDecesos=nHospitalizados=nDescartados=nRecuperados=nTPruebas=op=0;
+        do{
+            
+        }while(op!=5);
     }
 }
