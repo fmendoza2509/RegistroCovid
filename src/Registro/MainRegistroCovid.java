b87239193d8acd6f04aaf366f387a839f9517d05
frangusdj <frangusdj@hotmail.com>
2020-06-04 07:10:26 -0500

    Programa probado

--- src/Registro/MainRegistroCovid.java
+++ src/Registro/MainRegistroCovid.java
@@ -12,39 +12,40 @@ public class MainRegistroCovid {
         nContagiados=nDecesos=nHospitalizados=nDescartados=nRecuperados=nTPruebas=op=0;
         System.out.println("Ingrese el nombre del canton");
         NombreCanton = lector.nextLine();
-        do{
-            do{
-                System.out.println("Ingrese una de las siguientes opciones del menú");
-                System.out.println("(1) Para agregar parroquia");
-                System.out.println("(2) Para imprimir las métricas por parroquia");
-                System.out.println("(3) Para imprimir la métrica cantonal");
-                System.out.println("(4) Para salir");
-                op = lector.nextInt();
-                switch(op){
-                    case 1:
-                        oParr.Leer();
-                        nContagiados = nContagiados + oParr.getContagios();
-                        nHospitalizados = nHospitalizados + oParr.getHospitalizados();
-                        nRecuperados = nRecuperados + oParr.getRecuperados();
-                        nDecesos = nDecesos + oParr.getDecesos();
-                        nDescartados =  nDescartados + oParr.getDescartados();
-                        nTPruebas = nTPruebas + oParr.getTotalPruebas();
-                        oCant.AddPrimero(oParr);
+        do {
+            System.out.println("Ingrese una de las siguientes opciones del menú");
+            System.out.println("(1) Para agregar parroquia");
+            System.out.println("(2) Para imprimir las métricas por parroquia");
+            System.out.println("(3) Para imprimir la métrica cantonal");
+            System.out.println("(4) Para salir");
+            op = lector.nextInt();
+            switch (op) {
+                case 1:
+                    oParr.Leer();
+                    nContagiados = nContagiados + oParr.getContagios();
+                    nHospitalizados = nHospitalizados + oParr.getHospitalizados();
+                    nRecuperados = nRecuperados + oParr.getRecuperados();
+                    nDecesos = nDecesos + oParr.getDecesos();
+                    nDescartados = nDescartados + oParr.getDescartados();
+                    nTPruebas = nTPruebas + oParr.getTotalPruebas();
+                    oCant.AddPrimero(oParr);
                     break;
-                    case 2:
-                        oCant.Imprimir();
+                case 2:
+                    oCant.Imprimir();
                     break;
-                    case 3:
-                        System.out.println("Cantón: "+NombreCanton);
-                        System.out.println("Número de personas contagiadas: "+nContagiados);
-                        System.out.println("Número de personas hospitalizadas: "+nHospitalizados);
-                        System.out.println("Número de personas recuperadas: "+nRecuperados);
-                        System.out.println("Número de personas fallecidas: "+nDecesos);
-                        System.out.println("Número de personas descartadas: "+nDescartados);
-                        System.out.println("Número total de pruebas: "+nTPruebas);
+                case 3:
+                    System.out.println("Cantón: " + NombreCanton);
+                    System.out.println("Número de personas contagiadas: " + nContagiados);
+                    System.out.println("Número de personas hospitalizadas: " + nHospitalizados);
+                    System.out.println("Número de personas recuperadas: " + nRecuperados);
+                    System.out.println("Número de personas fallecidas: " + nDecesos);
+                    System.out.println("Número de personas descartadas: " + nDescartados);
+                    System.out.println("Número total de pruebas: " + nTPruebas);
                     break;
-                }
-            }while((op>0)&&(op<5));
-        }while(op!=4);
+                default:
+                    System.out.println("Escoja una opción válida y vuela a intentarlo");
+                    break;
+            }
+        } while (op != 4);
     }
 }
