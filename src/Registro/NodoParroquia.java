7d80fd99d9e4d657a74e21958626b1249017d4e3
frangusdj <frangusdj@hotmail.com>
2020-06-03 20:49:26 -0500

    NodoPrroquia programado

--- src/Registro/NodoParroquia.java
+++ src/Registro/NodoParroquia.java
@@ -1,21 +1,44 @@
 package registro;
 public class NodoParroquia {
-
+    /***********************************************************
+     *Declaramos un objeto donde se va a guardar la información*
+     *de cada parroquia                                        *
+     ***********************************************************/
     private Object valor;
-
+    /*************************************************************
+     *Declaramos un objeto del mismo tipo del nodo que almacenará*
+     *la referencia al siguiente nodo                            *
+     *************************************************************/
     private NodoParroquia sgte;
-
+    //Constructor
     public NodoParroquia(Object valor) {
+        setValor(valor);
+        setSgte(null);
     }
-
+    //Métodos Setters
+    public void setValor(Object valor) {
+        this.valor = valor;
+    }
+    public void setSgte(NodoParroquia sgte) {
+        this.sgte = sgte;
+    }
+    //Métodos Getters
+    public Object getValor() {
+        return valor;
+    }
+    public NodoParroquia getSgte() {
+        return sgte;
+    }
+    //Método para enlazar al siguiente nodo
     public void EnlazarSiguiente(NodoParroquia n) {
+        setSgte(n);
     }
-
+    //Método para que nos retorne el valor del siguiente nodo
     public NodoParroquia ObtenerSiguiente() {
-        throw new UnsupportedOperationException("Not supported yet.");
+        return getSgte();
     }
-
+    //Método para obtener el valor de este nodo
     public Object ObtenerValor() {
-        throw new UnsupportedOperationException("Not supported yet.");
+        return getValor();
     }
 }
