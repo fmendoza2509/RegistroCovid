f9bd037b8e77e71d00d5f3c70c6badb9c1c69d98
frangusdj <frangusdj@hotmail.com>
2020-06-03 23:19:17 -0500

    Menu contextual

--- src/Registro/MainRegistroCovid.java
+++ src/Registro/MainRegistroCovid.java
@@ -1,11 +1,32 @@
 package registro;
+
+import java.util.Scanner;
+
 public class MainRegistroCovid {
     public static void main(String[] args) {
+        Scanner lector = new Scanner(System.in);
         String NombreCanton="";
         int nContagiados,nDecesos,nHospitalizados,nDescartados,nRecuperados,nTPruebas,op;
         nContagiados=nDecesos=nHospitalizados=nDescartados=nRecuperados=nTPruebas=op=0;
+        System.out.println("Ingrese el nombre del canton");
+        NombreCanton = lector.nextLine();
         do{
-            
-        }while(op!=5);
+            do{
+                System.out.println("Ingrese una de las siguientes opciones del menú");
+                System.out.println("(1) Para agregar parroquia");
+                System.out.println("(2) Para imprimir las métricas por parroquia");
+                System.out.println("(3) Para imprimir la métrica cantonal");
+                System.out.println("(4) Para salir");
+                op = lector.nextInt();
+                switch(op){
+                    case 1:
+                    break;
+                    case 2:
+                    break;
+                    case 3:
+                    break;
+                }
+            }while((op>0)&&(op<5));
+        }while(op!=4);
     }
 }
